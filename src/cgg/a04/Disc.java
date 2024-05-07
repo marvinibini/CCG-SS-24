1a875ab5f478d492827f5ea6f660b500c857f6c5
marvin.chow99 <marvin.chow99@outlook.com>
2024-05-07 06:40:34 +0000

    hallo

--- src/cgg/a04/Disc.java
+++ src/cgg/a04/Disc.java
@@ -3,7 +3,7 @@ package cgg.a04;
 import cgg.a03.*;
 import cgtools.*;
 
-public record Disc(Point ankerpunkt, Direction normal, double radius, Color color) implements Shape {
+public record Disc(Point anchorPoint, Direction normal, double radius, Color color) implements Shape {
     public Hit intersect(Ray r) {
 
         Direction direction = r.direction();
@@ -13,10 +13,10 @@ public record Disc(Point ankerpunkt, Direction normal, double radius, Color colo
         double limitation = 0;
         double denominator = Vector.dotProduct(direction, normal);
 
-        double t = (Vector.dotProduct(Vector.subtract(ankerpunkt, x0), normal)) / (denominator);
+        double t = (Vector.dotProduct(Vector.subtract(anchorPoint, x0), normal)) / (denominator);
         if (r.isValid(t) == true) {
             Point treffer = Vector.add(Vector.multiply(direction, t), x0);
-            limitation = Math.abs(Vector.length(Vector.subtract(treffer, ankerpunkt)));
+            limitation = Math.abs(Vector.length(Vector.subtract(treffer, anchorPoint)));
             if (limitation < radius) {
                 hit = new Hit(t, treffer, normal, color);
             }
